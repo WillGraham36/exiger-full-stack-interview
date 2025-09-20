c736dafaebfc3e872db5b8bbd8c9f2ad201b7eb6
Thomas Huitema <thuitema35@gmail.com>
2025-09-19 20:54:45 -0400

    Update backend folders to remove update_ratings endpoint

--- backend/express-js/server.js
+++ backend/express-js/server.js
@@ -1,59 +1,54 @@
-const express = require("express");
-const app = express();
-app.use(express.json());
-
-
 /*
 We are building a movie database and need to complete a couple of API endpoints
 We won't connect to a real database here, so we want you to create a data structure to mimic one
 
 Movies have the following attributes:
 {
-  "id": int,
-  "title": string,
-  "year": int,
-  "director": string,
-  "rating": float (1-5 inclusive),
-  "genre": string (must be one of: action, horror, drama, sci-fi)
+    "id": int,
+    "title": string,
+    "year": int,
+    "director": string,
+    "rating": float (1-5 inclusive),
+    "num_ratings": int (>= 0)
 }
 */
 
+const express = require("express");
+const app = express();
+app.use(express.json());
 
 // Create your data structure(s) here:
-
 
 // Complete the API endpoints below:
 
 /*
-  Add movie to the database
-  Request body: {
-    "title": string,
-    "year": int,
-    "director": string,
-    "rating": float (1-5 inclusive)
-    "genre": string (must be one of: action, horror, drama, sci-fi)
-  }
+POST /movies -- Add movie to the database
+Request body: {
+    “title”: string,
+    “year”: int,
+    “director”: string,
+}
 
-  Response:
-  If valid: 201 status code and JSON {"id": <movie id>}
-  If invalid: 400 status code and JSON {"error": <message>}
-  If duplicate of a previous entry: return 409 status code and JSON {"error": <message>"}
+Response:
+If valid: 201 status code and JSON {“id”: <movie id>}
+If invalid: 400 status code and JSON {“error”: <message>}
+If duplicate of a previous entry: 409 status code and JSON {“error”: <message>”}
 */
 app.post("/movies", (req, res) => {
   res.status(501).json({ error: "Not implemented" });
 });
 
-
-
 /*
-  Get all movies
-  BONUS: add query parameter filter movies by title
+GET /movies -- Get all movies
+BONUS: add query parameter to filter movies by title (e.g. /movies?title=Inception)
 
-  Response: status code 200, JSON: 
-  {
-    { "id": 1, "title": "Inception", "year": 2010, "director": "Christopher Nolan", "genre": "Sci-Fi", "rating": 4.98},
-    ...
-  }
+Response: status code 200, JSON:
+{
+    items: [
+        { "id": 1, "title": "Inception", "year": 2010, "director": "Christopher Nolan", "rating": 4.98, "num_ratings": 5},
+        …
+    ]
+}
 */
 app.get("/movies", (req, res) => {
   res.status(501).json({ error: "Not implemented" });
@@ -62,15 +57,13 @@ app.get("/movies", (req, res) => {
 
 
 /*
-  Update movie rating
-  Request body:
-  {
-    "ratings": [3, 4.9, 1, 2, 4.0]
-  }
+PUT /movies/{movie_id}/rating -- Recalculate movie's ratings field given new ratings, updating rating and num_rating attributes
+Example path: /movies/33/rating, where 33 is a movie_id
+Request body: {“ratings”: [3, 4.9, 1, 2, 4.0]}
 
-  Response:
-  If valid: 200 status code, JSON {"id": <movie_id>, "rating": <rating>}
-  If input invalid: 400 status code, JSON {"error": <msg>)}
+Response:
+If valid: 200 status code, JSON {“id”: <movie_id>, “rating”: <rating>, "num_ratings": <num_ratings>}
+If input invalid or movie_id doesn't exist: 400 status code, JSON {“error”: <msg>)
 */
 app.put("/movies/:id/rating", (req, res) => {
   res.status(501).json({ error: "Not implemented" });
--- backend/fastapi/main.py
+++ backend/fastapi/main.py
@@ -1,13 +1,60 @@
 """
+We are building a movie database and need to complete a some API endpoints
+We won't connect to a real database here, so we want you to create a data structure to mimic one
+
+Movies have the following attributes:
+{
+    "id": int,
+    "title": string,
+    "year": int,
+    "director": string,
+    "rating": float (1-5 inclusive),
+    "num_ratings": int (>= 0)
+}
+
 To run:
 1. cd fastapi
 2. `fastapi dev main.py`
 """
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Create your data structure(s) here:
+
+# Complete the API endpoints below:
+
+"""
+POST /movies -- Add movie to the database
+Request body: {
+    “title”: string,
+    “year”: int,
+    “director”: string
+}
+
+Response:
+If valid: 201 status code and JSON {“id”: <movie id>}
+If invalid: 400 status code and JSON {“error”: <message>}
+If duplicate of a previous entry: 409 status code and JSON {“error”: <message>”}
+"""
+# TODO: add route decorator
+def add_movie():
+    pass
+
+"""
+GET /movies -- Get all movies
+BONUS: add query parameter to filter movies by title (e.g. /movies?title=Inception)
+
+Response: status code 200, JSON: 
+{ 
+    items: [
+        { "id": 1, "title": "Inception", "year": 2010, "director": "Christopher Nolan", "rating": 4.98, "num_ratings": 5},
+        …
+    ]
+}
+"""
+# TODO: add route decorator
+def get_movies():
+    pass
